4ce4c876e60c6de388569b68d05f8e0ff23cc585
liu <liuweicheng2019@126.com>
2019-11-03 22:43:52 +0800

    查看用户列表

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = '刘韦成';
+        $user->email = 'wutong9802@qq.com';
+        $user->save();
+    }
+}
