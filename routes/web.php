79c2e9240595a0acc7b16da54f54a78116ee3825
liu <liuweicheng2019@126.com>
2019-11-03 21:59:46 +0800

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@
 */
 
 // Route::get('/', function () {return view('welcome');});
-
+//用户访问
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+//用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 //Route::get('/users', 'UsersController@index')->name('users.index');
@@ -27,6 +28,10 @@ Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 //Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
+//用户登陆
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+//用户编辑资料/CURD
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
