18a65d6eed235ea818fd82a765f1487c1f8d580f
liu <liuweicheng2019@126.com>
2019-11-03 10:51:37 +0800

    基础

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 @yield('content')
