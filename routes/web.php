422a009ffbba891bc98abf4bb5f41082b1d47176
liu <liuweicheng2019@126.com>
2019-11-03 22:18:32 +0800

    访问策略

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,15 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 //用户注册
 Route::get('signup', 'UsersController@create')->name('signup');
+
 Route::resource('users', 'UsersController');
 //Route::get('/users', 'UsersController@index')->name('users.index');
 //Route::get('/users/create', 'UsersController@create')->name('users.create');
+//用户个人主页
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 //Route::post('/users', 'UsersController@store')->name('users.store');
-//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//用户编辑资料/CURD
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 //Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 //Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
@@ -33,5 +36,4 @@ Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
-//用户编辑资料/CURD
-Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+
